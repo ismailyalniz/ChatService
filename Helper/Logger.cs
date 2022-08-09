bfe74b806fbe94af1a690c5319f6d374ce85faa8
ismail yalniz <cemyalniz@hotmail.com>
2022-08-09 16:00:49 +0300

    Server and client applications are completed without unit test.

---
+++ Helper/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+    public static class Logger
+    {
+        public static void Log(Exception exception)
+        {
+            //ToDo: Logging errors.
+        }
+    }
+}
